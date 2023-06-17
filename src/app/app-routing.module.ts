2905d0b8be79ce166ecd0ca30b423da02ed9413a
David Quiroga <97131802+David-Quiroga@users.noreply.github.com>
2023-06-17 17:56:43 -0500

    API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,50 +1,25 @@
-// import { NgModule } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule, Routes } from '@angular/router';
-// import { LoginComponent } from './auth/login/login.component';
-// import { RegisterComponent } from './auth/register/register.component';
-// import { DashboardComponent } from './pages/dashboard/dashboard.component';
-// import { ProductComponent } from './pages/product/product.component';
-// import { CategoryComponent } from './pages/category/category.component';
-// import { NotFoundComponent } from './pages/not-found/not-found.component';
-// import { AuthRoutingModule } from './auth/auth-routing/auth-routing.module';
-// import { PagesModule } from './pages/pages.module';
-
-// const routes: Routes = [
-//   { path: 'login', component: LoginComponent },
-//   { path: 'register', component: RegisterComponent },
-//   { path: 'dashboard', component: DashboardComponent },
-//   { path: 'product', component: ProductComponent },
-//   { path: 'category', component: CategoryComponent },
-//   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-//   { path: '**', component: NotFoundComponent },
-// ];
-
-// @NgModule({
-//   declarations: [],
-//   imports: [CommonModule, RouterModule.forRoot(routes), PagesModule],
-//   exports: [RouterModule, AuthRoutingModule, PagesModule],
-// })
-// export class AppRoutingModule {}
-
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { NoPagesFoundComponent } from './pages/no-pages-found/no-pages-found.component';
 import { AuthRoutingModule } from './auth/auth-routing.module';
 import { PagesRoutingModule } from './pages/pages-routing.module';
-import { NoPagesFoundComponent } from './no-pages-found/no-pages-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NoPagesFoundComponent },
-]
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
-    AuthRoutingModule,
-    PagesRoutingModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule]
+  exports: [
+    RouterModule,
+    AuthRoutingModule,
+    PagesRoutingModule],
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
-import { NoPagesFoundComponent } from './no-pages-found/no-pages-found.component';
+import { NoPagesFoundComponent } from './pages/no-pages-found/no-pages-found.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from "./shared/shared.module";
 
